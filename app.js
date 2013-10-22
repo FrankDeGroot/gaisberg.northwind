96d5afbd3d2b125bd3c493d7a9fdae3636967928
Frank de Groot - Schouten <fjtdg+github@hotmail.com>
2013-10-22 20:10:38 +0200

    Mock angularjs.

---
+++ app.js
@@ -0,0 +1,12 @@
+﻿angular.module('gaisberg', []).controller('todoCtrl', function ($scope) {
+    $scope.addTodo = function () {
+        alert('Add Todo');
+    };
+    $scope.deleteTodo = function () {
+        alert('Delete Todo');
+    };
+    $scope.newTodo = '';
+    $scope.todos = [
+        { text: 'Test', complete: false }
+    ];
+});
