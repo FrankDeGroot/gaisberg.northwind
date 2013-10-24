4c4780ec1d045ffb8c9856d649014f554269ab40
Frank de Groot - Schouten <fjtdg+github@hotmail.com>
2013-10-24 23:30:49 +0200

    Todos opgehaald en getoond met AngularJS.

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-﻿angular.module('gaisberg', []).controller('todoCtrl', function ($scope) {
+﻿angular.module('gaisberg', []).controller('todoCtrl', function ($scope, $q) {
+    var client = new WindowsAzure.MobileServiceClient('https://gaisberg.azure-mobile.net/', 'zWkZwDXaswoZZUHPHUMQqoNdCeHuad42'),
+        todoItemTable = client.getTable('todoitem');
+    var query = todoItemTable.where({ complete: false });
+    query.read().then(function (todos) {
+       $scope.$apply(function () {
+           $scope.todos = todos;
+       });
+    });
+
     $scope.addTodo = function () {
         alert('Add Todo');
     };
@@ -6,7 +15,5 @@
         alert('Delete Todo');
     };
     $scope.newTodo = '';
-    $scope.todos = [
-        { text: 'Test', complete: false }
-    ];
+    $scope.todos = [];
 });
